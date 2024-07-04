8a2315f301cebc4e7cdc09457e83f51589fd28d6
LukaianG <Lukaianulguim@gmail.com>
2024-07-04 16:03:42 -0300

    Add files via upload

---
+++ mouses.js
@@ -0,0 +1,20 @@
+function sobre(){
+    document.getElementById("cores").style.backgroundColor="black";
+    document.getElementById("log").innerHTML += "sobre a div / cor: preto<br>";
+}
+
+function saindo(){
+    document.getElementById("cores").style.backgroundColor="#00a4e1";
+    document.getElementById("log").innerHTML += "saindo da div / cor: azul<br>";
+}
+
+
+function tsisob(){
+    document.getElementById("tsi").style.backgroundColor="black";
+    document.getElementById("log").innerHTML += "sobre a div / cor: preto<br>";
+}
+
+function tsisai(){
+    document.getElementById("tsi").style.backgroundColor="#00a4e1";
+    document.getElementById("log").innerHTML += "saindo da div / cor: azul<br>";
+}
